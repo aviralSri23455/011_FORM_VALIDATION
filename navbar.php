80f157d757be0e3e3b8a0c0a48d382c7169a69ef
aviralinfyx <aviralinfyxpert@gmail.com>
2024-04-18 18:17:59 +0530

    FORM_VALIDATION_PHP

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498db;">
+  <div class="container-fluid">
+
+  <h1><a class="navbar-brand" href="index.php" id="navbar-logo">Logo</a></h1>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="welcome.php"></a>
+        </li>
+    </ul>
+    <form class="d-flex">
+        <a class="btn btn-custom1 mx-2" type="submit" href="signup.php">Signup</a>
+        <a class="btn btn-custom2 mx-2" type="submit" href="login.php">Login</a>
+        <a class="btn btn-custom3 mx-2" type="submit" href="logout.php">Logout</a>
+    </form>
+  </div>
+</nav>
